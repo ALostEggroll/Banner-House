8e0091217bc63e41b53468ef0c23014fb9798a5e
Dexter Estrada <dexter.estrada99@gmail.com>
2022-07-10 19:59:52 -0700

    Preparing to track multiple units

--- Banner House/Assets/scripts/CharacterCombatController.cs
+++ Banner House/Assets/scripts/CharacterCombatController.cs
@@ -6,18 +6,23 @@ using UnityEngine.AI;
 /*
  * This class controls unit behavior in combat
  */
+[RequireComponent(typeof(NavMeshAgent))]
 public class CharacterCombatController : MonoBehaviour
 {
-    [SerializeField] Transform currentTarget;    // Current targeted unit
+    Transform currentTarget;    // Current targeted unit
     NavMeshAgent agent;         // This unit
+    public team team { get; set; }
 
     void Awake()
     {
         // Choosing this unit as agent
         agent = GetComponent<NavMeshAgent>();
 
+        // Adding to CombatManager
+        CombatManager.Instance.addUnit(this);
+
         // Finding closest target and choose target
-        //currentTarget = CombatManager.instance.enemy.transform;
+        currentTarget = CombatManager.Instance.getEnemy(this)[0].transform;
     }
 
     void Update()
@@ -25,18 +30,29 @@ public class CharacterCombatController : MonoBehaviour
         agent.SetDestination(currentTarget.position);
     }
 
-    /*
     // This function finds the closest enemy to this unit
-    Transform findclosest(GameObject enemy)
+    Transform findclosest(List<CharacterCombatController> enemy)
     {
-        Transform closestUnit = null;
-        float distance;
+        if (enemy == null)
+        { 
+            return null;
+        }
+
+        Transform closestUnit = enemy[0].transform;
+        //float distance = Vector3.Distance(closestUnit);
+        /*
         foreach (GameObject obj in objects)
         {
             float temp = Vector3.Distance(obj.transform.position, transform.position);
             if (temp < distance)
                 distance = temp;
         }
+        */
+        return closestUnit;
     }
-    */
+}
+public enum team
+{
+    team1,
+    team2,
 }
--- Banner House/Assets/scripts/CombatManager.cs
+++ Banner House/Assets/scripts/CombatManager.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 public class CombatManager : MonoBehaviour
 {
     #region Singleton
-    public static CombatManager instance;
+    private static CombatManager _instance;
+    public static CombatManager Instance
+    {
+        get { return _instance; }
+        private set { _instance = value; }
+    }
 
     private void Awake()
     {
-        instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+            return;
+        }
+
+        Destroy(gameObject);
     }
     #endregion
 
-    [SerializeField] public GameObject ally;
-    [SerializeField] public GameObject enemy;
+    [SerializeField] public List<CharacterCombatController> team1 = new List<CharacterCombatController>();
+    [SerializeField] public List<CharacterCombatController> team2 = new List<CharacterCombatController>();
+
+    /*
+     * Sorts the NavMesh based on what team they're on
+     */
+    public void addUnit(CharacterCombatController unit)
+    {
+        switch (unit.team)
+        {
+            case team.team1:
+                team1.Add(unit);
+                break;
+            case team.team2:
+                team2.Add(unit);
+                break;
+        }
+    }
+
+    /*
+     * Returns a list of the enemy
+     */
+    public List<CharacterCombatController> getEnemy(CharacterCombatController unit)
+    {
+        switch (unit.team)
+        {
+            case team.team1:
+                return team2;
+            case team.team2:
+                return team1;
+        }
+        return null;
+    }
 }
